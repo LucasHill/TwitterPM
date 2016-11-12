f4b59720a7c7c392a53f1ed6ad0f5eb28c578d39
Lucas Hill <kingofallhills@gmail.com>
2016-11-12 11:09:02 -0800

    Add support for sending direct messages.

--- test/twitterClientTest.js
+++ test/twitterClientTest.js
@@ -11,7 +11,7 @@ describe('Twitter Client', function() {
       const request = twitterClient._assembleRequest('http://www.google.com', queryObj);
 
       request.should.have.property('url').equal('http://www.google.com');
-      request.should.have.property('qs').equal('key1=a&key2=b');
+      request.should.have.property('qs').equal(queryObj);
     });
 
     it('get the oauth values from config', function() {
@@ -19,10 +19,10 @@ describe('Twitter Client', function() {
 
       const oauth = twitterClient._assembleRequest('http://www.google.com', queryObj).oauth;
 
-      oauth.should.have.property('consumerKey').equal('consumer key!');
-      oauth.should.have.property('consumerSecret').equal('consumer secret!');
+      oauth.should.have.property('consumer_key').equal('consumer key!');
+      oauth.should.have.property('consumer_secret').equal('consumer secret!');
       oauth.should.have.property('token').equal('the token!');
-      oauth.should.have.property('tokenSecret').equal('the token secret!');
+      oauth.should.have.property('token_secret').equal('the token secret!');
     });
   });
 });
