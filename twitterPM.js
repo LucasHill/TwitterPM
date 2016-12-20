42fe22cfdfa65defbda3241adffef18e3550a3c2
Lucas Hill <kingofallhills@gmail.com>
2016-12-19 23:53:17 -0800

    Bot responds when followed.

---
+++ twitterPM.js
@@ -0,0 +1,36 @@
+"use strict";
+
+const client = require('./lib/twitterClient.js');
+const config = require('config');
+const logger = require('./lib/logger');
+
+const botQueue = require('kue').createQueue({
+  prefix: 'botQueue',
+  redis: {
+    host: config.get('Redis.host'),
+    port: config.get('Redis.port'),
+    auth: config.get('Redis.password')
+  }
+});
+
+const KueManager = require('./lib/kueManager');
+const kue = new KueManager(botQueue);
+
+initUserStream();
+processFollowCommands();
+
+
+function initUserStream() {
+  const apiConstants = require('./lib/apiConstants');
+  const StreamParser = require('./lib/twitterStreamParser');
+  const stream = new StreamParser(apiConstants.USER_STREAM, kue);
+  stream.initStream();
+}
+
+function processFollowCommands() {
+  botQueue.process('FOLLOW', async function(job, done){
+    logger.info(`Job ${job.id} started.`);
+    await client.directMessage('Thanks for the follow!', job.data.target);
+    done();
+  });
+}
